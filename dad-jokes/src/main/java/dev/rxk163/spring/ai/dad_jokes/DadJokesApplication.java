fce9354e75f6b0aa20f69ec6c7e43a9dc9cc4563
tagore <krtagorek@gmail.com>
2024-10-29 13:06:40 -0500

    Spring AI project

---
+++ dad-jokes/src/main/java/dev/rxk163/spring/ai/dad_jokes/DadJokesApplication.java
@@ -0,0 +1,13 @@
+package dev.rxk163.spring.ai.dad_jokes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadJokesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DadJokesApplication.class, args);
+	}
+
+}
---
+++ dad-jokes/src/test/java/dev/rxk163/spring/ai/dad_jokes/DadJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rxk163.spring.ai.dad_jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
