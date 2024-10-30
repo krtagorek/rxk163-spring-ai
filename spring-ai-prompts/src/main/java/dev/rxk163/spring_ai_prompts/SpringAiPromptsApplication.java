040cecb94b3991de558f8643eff0b15af904ac6b
tagore <krtagorek@gmail.com>
2024-10-30 13:04:14 -0500

    Spring AI, Implemented Prompt Template

---
+++ spring-ai-prompts/src/main/java/dev/rxk163/spring_ai_prompts/SpringAiPromptsApplication.java
@@ -0,0 +1,13 @@
+package dev.rxk163.spring_ai_prompts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPromptsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiPromptsApplication.class, args);
+	}
+
+}
---
+++ spring-ai-prompts/src/test/java/dev/rxk163/spring_ai_prompts/SpringAiPromptsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rxk163.spring_ai_prompts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPromptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
